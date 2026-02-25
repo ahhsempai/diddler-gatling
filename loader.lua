efb0321195eab263583741206cc5fa7bf85805b2
ahhsempai <128315878+ahhsempai@users.noreply.github.com>
2026-02-25 12:39:15 +0500

    Delete loader.lua

--- loader.lua
+++
@@ -1,7 +0,0 @@
-local s,u=game:HttpGet("https://raw.githubusercontent.com/ahhsempai/diddler-gatling/main/diddler.lua")
-local f=loadstring(s)
-if type(f)\~="function" then
-    -- Hercules fix: force eval
-    f=loadstring(s:gsub("return%s*(%b())","return %1()")) or error("Hercules broke loadstring")
-end
-return f()
